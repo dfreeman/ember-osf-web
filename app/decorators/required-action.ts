2a24ba0c2aa403561a5a186fad8538398fe9ca95
Dan Freeman <dfreeman@salsify.com>
2018-10-16 17:31:27 +0200

    Fix `requiredAction` decorator

--- app/decorators/required-action.ts
+++ app/decorators/required-action.ts
@@ -18,8 +18,20 @@ import { assert } from '@ember/debug';
 export default function requiredAction(
     classPrototype: any,
     propertyName: string,
+    /* tslint:disable-next-line trailing-comma */
+    ...additional: any[]
 ): any {
     const className = classPrototype.constructor.name;
+
+    // TypeScript's decorator signature doesn't include the property initializer,
+    // but Babel's actual implementation will overwrite a preexisting passed-in
+    // value unless we set an initializer that defaults to that existing value.
+    if (!additional[0].initializer) {
+        // eslint-disable-next-line no-param-reassign
+        additional[0].initializer = function() {
+            return this[propertyName];
+        };
+    }
 
     // eslint-disable-next-line no-param-reassign
     classPrototype[propertyName] = () => {
