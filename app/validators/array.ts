40c62970ef4206aeb902ff4f0701dc06a184bce6
Dan Freeman <dfreeman@salsify.com>
2018-10-16 17:27:53 +0200

    Use ES class for `array` validator implementation

--- app/validators/array.ts
+++ app/validators/array.ts
@@ -5,19 +5,7 @@ interface Options extends EmberObject {
     minLength?: number;
 }
 
-const ArrayValidator = BaseValidator.extend({
-    validate<T>(value: T[], options: Options): string | true {
-        const minLength = get(options, 'minLength');
-
-        if (minLength && value.length < minLength) {
-            return this.createErrorMessage('minLength', value, options);
-        }
-
-        return true;
-    },
-});
-
-ArrayValidator.reopenClass({
+export default class ArrayValidator extends BaseValidator {
     /**
      * Define attribute specific dependent keys for your validator
      *
@@ -29,9 +17,17 @@ ArrayValidator.reopenClass({
      * @param {String} attribute The attribute being evaluated
      * @return {Array}
      */
-    getDependentsFor(attribute: string): string[] {
+    static getDependentsFor(attribute: string): string[] {
         return [`model.${attribute}.length`];
-    },
-});
+    }
 
-export default ArrayValidator;
+    validate<T>(value: T[], options: Options): string | true {
+        const minLength = get(options, 'minLength');
+
+        if (minLength && value.length < minLength) {
+            return this.createErrorMessage('minLength', value, options);
+        }
+
+        return true;
+    }
+}
